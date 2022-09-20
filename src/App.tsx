eccb8fe669672ac658c6597c77ea82ee1a48bf23
Jay <chriese0112@gmail.com>
2022-09-20 10:28:41 +0800

    Add Footer file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import { Outlet } from 'react-router-dom';
 import { ResetStyle, GlobalStyle } from './components/ResetStyle';
 // import { Counter } from './features/counter/Counter';
@@ -11,6 +12,7 @@ function App() {
 			<GlobalStyle />
 			<Header />
 			<Outlet />
+			<Footer />
 		</>
 	);
 }

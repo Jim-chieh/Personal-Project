9b748c07caa2179eaf68fc1a909f7dba1ed2b934
Jay <chriese0112@gmail.com>
2022-10-17 13:18:36 +0800

    Add storybook components

---
+++ src/stories/IssuePageFixedTitleNotplanned.stories.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import IssuePageFixedTtitle from '../pages/IssuePage/IssuePageFixedTitle';
+import '../../.storybook/header.css';
+import { Provider } from 'react-redux';
+import { store } from '../redux/store';
+
+export default {
+	title: 'IssuePage/FixedTitle/not_planned',
+	component: IssuePageFixedTtitle,
+	parameters: {
+		layout: 'fullscreen'
+	}
+} as ComponentMeta<typeof IssuePageFixedTtitle>;
+
+const Template: ComponentStory<typeof IssuePageFixedTtitle> = args => (
+	<Provider store={store}>
+		<div
+			style={{
+				margin: '20px',
+				padding: '70px'
+			}}
+		>
+			<IssuePageFixedTtitle {...args} />
+		</div>
+	</Provider>
+);
+
+export const IssuePageFixedTtitleStories = Template.bind({});
+IssuePageFixedTtitleStories.args = {
+	title: 'Storybook',
+	number: 70,
+	author: 'Jim-chieh',
+	comments: 7,
+	$created_at: '2022-10-01T08:54:53',
+	currentState: 'not_planned'
+};
